464ff04ce6f37c28ce192c10e0c1bf45fa9a8e74
NatureFreshMilk <naturefreshmilk@github.com>
2019-03-01 11:50:02 +0100

    csv stub

--- export/csv.lua
+++ export/csv.lua
@@ -1,5 +1,20 @@
 
+local export_csv = function()
+  local data = ""
+
+  for _, metric in ipairs(monitoring.metrics) do
+    -- TODO
+  end
+end
+
 
 monitoring.csv_init = function()
-  -- TODO
+  local timer = 0
+  minetest.register_globalstep(function(dtime)
+    timer = timer + dtime
+    if timer < 5 then return end
+    timer=0
+
+    export_csv()
+  end)
 end
