9621258905fb12d6f6f420c9f3fd03b87626a261
NatureFreshMilk <naturefreshmilk@github.com>
2019-04-16 11:39:18 +0200

    builtin_disable setting

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ monitoring = {
   settings = {
     prom_push_url = minetest.settings:get("monitoring.prometheus_push_url"),
     prom_push_key = minetest.settings:get("monitoring.prometheus_push_key") or "",
+    builtin_disable = minetest.settings:get("monitoring.builtin_disable"),
     csv_enable = minetest.settings:get("monitoring.csv_enable"),
     json_enable = minetest.settings:get("monitoring.json_enable")
   }
@@ -20,27 +21,31 @@ dofile(MP.."/export/prometheus_push.lua")
 dofile(MP.."/export/csv.lua")
 dofile(MP.."/export/json.lua")
 
---dofile(MP.."/builtin/mods.lua")
-dofile(MP.."/builtin/abm_calls.lua")
-dofile(MP.."/builtin/auth_fail.lua")
-dofile(MP.."/builtin/chat_count.lua")
-dofile(MP.."/builtin/cheat_count.lua")
-dofile(MP.."/builtin/craft_count.lua")
-dofile(MP.."/builtin/dig_count.lua")
-dofile(MP.."/builtin/eat_count.lua")
-dofile(MP.."/builtin/generated.lua")
-dofile(MP.."/builtin/join_count.lua")
-dofile(MP.."/builtin/lag.lua")
-dofile(MP.."/builtin/lbm_calls.lua")
-dofile(MP.."/builtin/place_count.lua")
-dofile(MP.."/builtin/nodetimer_calls.lua")
-dofile(MP.."/builtin/playercount.lua")
-dofile(MP.."/builtin/protection_violation_count.lua")
-dofile(MP.."/builtin/punchplayer.lua")
-dofile(MP.."/builtin/received_fields.lua")
-dofile(MP.."/builtin/registered_count.lua")
-dofile(MP.."/builtin/time.lua")
-dofile(MP.."/builtin/uptime.lua")
+if not monitoring.settings.builtin_disable then
+  print("[monitoring] registering builtin metrics")
+
+  --dofile(MP.."/builtin/mods.lua")
+  dofile(MP.."/builtin/abm_calls.lua")
+  dofile(MP.."/builtin/auth_fail.lua")
+  dofile(MP.."/builtin/chat_count.lua")
+  dofile(MP.."/builtin/cheat_count.lua")
+  dofile(MP.."/builtin/craft_count.lua")
+  dofile(MP.."/builtin/dig_count.lua")
+  dofile(MP.."/builtin/eat_count.lua")
+  dofile(MP.."/builtin/generated.lua")
+  dofile(MP.."/builtin/join_count.lua")
+  dofile(MP.."/builtin/lag.lua")
+  dofile(MP.."/builtin/lbm_calls.lua")
+  dofile(MP.."/builtin/place_count.lua")
+  dofile(MP.."/builtin/nodetimer_calls.lua")
+  dofile(MP.."/builtin/playercount.lua")
+  dofile(MP.."/builtin/protection_violation_count.lua")
+  dofile(MP.."/builtin/punchplayer.lua")
+  dofile(MP.."/builtin/received_fields.lua")
+  dofile(MP.."/builtin/registered_count.lua")
+  dofile(MP.."/builtin/time.lua")
+  dofile(MP.."/builtin/uptime.lua")
+end
 
 if monitoring.settings.prom_push_url then
   if not http then
