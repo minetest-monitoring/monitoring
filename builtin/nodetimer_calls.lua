fa8ade52122ab2069330c81df5cfdab70e8ecb4b
NatureFreshMilk <naturefreshmilk@github.com>
2019-04-17 14:32:33 +0200

    fix stopping nodetimer

--- builtin/nodetimer_calls.lua
+++ builtin/nodetimer_calls.lua
@@ -8,9 +8,11 @@ minetest.register_on_mods_loaded(function()
       node.on_timer = function(pos, elapsed)
         metric.inc()
         local t0 = minetest.get_us_time()
-        old_action(pos, elapsed)
+        local result = old_action(pos, elapsed)
         local t1 = minetest.get_us_time()
+
         metric_time.inc(t1 - t0)
+        return result
       end
     end
   end
