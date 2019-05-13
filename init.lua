da19cb8bc1eff88c2dc1296275b1406d3f28df59
NatureFreshMilk <naturefreshmilk@github.com>
2019-05-13 08:37:09 +0200

    is_protected_count metric

--- init.lua
+++ init.lua
@@ -35,6 +35,7 @@ if not monitoring.settings.builtin_disable then
   dofile(MP.."/builtin/dig_count.lua")
   dofile(MP.."/builtin/eat_count.lua")
   dofile(MP.."/builtin/generated.lua")
+  dofile(MP.."/builtin/is_protected_count.lua")
   dofile(MP.."/builtin/join_count.lua")
   dofile(MP.."/builtin/lag.lua")
   dofile(MP.."/builtin/lbm_calls.lua")
