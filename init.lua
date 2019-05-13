9698de6654fce8f03baa04a0ed391200fffe305b
Thomas Rudin <thomas@rudin.io>
2019-05-13 18:28:30 +0200

    v1.02

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 
 monitoring = {
   version_major = 1,
-  version_minor = 1,
+  version_minor = 2,
   metrics = {}, -- {name="", help="", type="", ...}
   settings = {
     prom_push_url = minetest.settings:get("monitoring.prometheus_push_url"),
