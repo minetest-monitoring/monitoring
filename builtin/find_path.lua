99d2b15d5338e4abaadc658d35b733b00fe60303
NatureFreshMilk <naturefreshmilk@github.com>
2019-06-12 07:32:44 +0200

    simpler call count and time intercept

--- builtin/find_path.lua
+++ builtin/find_path.lua
@@ -1,11 +1,4 @@
 local metric = monitoring.counter("find_path_calls", "number of find_path calls")
 local metric_time = monitoring.counter("find_path_time", "time usage in microseconds for find_path calls")
 
-local old_find_path = minetest.find_path
-minetest.find_path = function(...)
-  metric.inc()
-  local t0 = minetest.get_us_time()
-  old_find_path(...)
-  local t1 = minetest.get_us_time()
-  metric_time.inc(t1 - t0)
-end
+minetest.find_path = metric.wrap(metric_time.wrap(minetest.find_path))
