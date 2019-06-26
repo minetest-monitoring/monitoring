1c519c415062641c972aa082133e554feb86c776
NatureFreshMilk <naturefreshmilk@github.com>
2019-06-26 12:47:41 +0200

    new player count (prejoin)

--- builtin/join_count.lua
+++ builtin/join_count.lua
@@ -1,8 +1,13 @@
 local metric_join = monitoring.counter("player_join_count", "number of players joined")
+local metric_join_new = monitoring.counter("player_join_new_count", "number of new players joined")
 local metric_prejoin = monitoring.counter("player_prejoin_count", "number of players prejoined")
 
 minetest.register_on_prejoinplayer(function(name, ip)
 	metric_prejoin.inc()
+
+	if not minetest.player_exists(name) then
+		metric_join_new.inc()
+	end
 end)
 
 minetest.register_on_joinplayer(function(player)
