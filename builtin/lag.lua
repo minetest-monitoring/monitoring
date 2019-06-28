9fefdcf4dfb3621b3c4d5dca104eb2c17c593d67
NatureFreshMilk <naturefreshmilk@github.com>
2019-06-28 07:47:39 +0200

    use setmax/min for lag stats

--- builtin/lag.lua
+++ builtin/lag.lua
@@ -6,12 +6,6 @@ local lag_histogram = monitoring.histogram("lag", "lag histogram",
   {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0})
 
 
-local min_lag
-local max_lag
-
-min_lag = 10
-max_lag = 0
-
 local function explode(sep, input)
         local t={}
         local i=0
@@ -31,32 +25,23 @@ local timer = 0
 local last_call_t = minetest.get_us_time()
 
 minetest.register_globalstep(function(dtime)
-  -- calculate own delta-time
-  local now = minetest.get_us_time()
-  local deltat = (now - last_call_t) / 1000000
-  -- swap timestamps
-  last_call_t = now
+	-- calculate own delta-time
+	local now = minetest.get_us_time()
+	local deltat = (now - last_call_t) / 1000000
 
-  lag_histogram.observe(deltat)
+	-- swap timestamps
+	last_call_t = now
 
-  -- executed on every step
-	if deltat > max_lag then
-		max_lag = deltat
-	end
+	lag_histogram.observe(deltat)
 
-	if deltat < min_lag then
-		min_lag = deltat
-	end
+	-- executed on every step
+	max_metric.setmax( deltat )
+	min_metric.setmin( deltat )
 
 	timer = timer + deltat
 	if timer < 5 then return end
 	timer=0
 
-  -- executed every few seconds
-  max_metric.set( max_lag )
-	min_metric.set( min_lag )
+	-- executed every few seconds
 	avg_metric.set( tonumber(get_max_lag()) )
-
-	min_lag = 10
-	max_lag = 0
 end)
