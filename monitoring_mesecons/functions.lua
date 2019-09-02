833f3bde4d430fdf0eb47a072cb528bccca47a32
NatureFreshMilk <naturefreshmilk@github.com>
2019-09-02 13:49:57 +0200

    function metrics

--- monitoring_mesecons/functions.lua
+++ monitoring_mesecons/functions.lua
@@ -1,34 +1,40 @@
 
 
--- function metrics
-local metric_function_change_count = monitoring.counter("mesecons_function_change_count", "number of change function executes")
-local metric_function_activate_count = monitoring.counter("mesecons_function_activate_count", "number of activate function executes")
-local metric_function_deactivate_count = monitoring.counter("mesecons_function_deactivate_count", "number of deactivate function executes")
-local metric_function_receptor_on_count = monitoring.counter("mesecons_function_receptor_on_count", "number of receptor_on function executes")
-local metric_function_receptor_off_count = monitoring.counter("mesecons_function_receptor_off_count", "number of receptor_off function executes")
-local metric_function_lc_interrupt_count = monitoring.counter("mesecons_function_lc_interrupt_count", "number of lc_interrupt function executes")
-local metric_function_lc_digiline_relay_count = monitoring.counter("mesecons_function_lc_digiline_relay_count", "number of lc_digiline_relay function executes")
-local metric_function_other = monitoring.counter("mesecons_function_other", "number of other function executes")
+local metrics = {} -- { { count=, time= }, ... }
+
+
+function register_function(name)
+	local entry = {}
+	entry.count = monitoring.counter("mesecons_function_" .. name .. "_count",
+		"number of " .. name .. " function executes")
+	entry.time = monitoring.counter("mesecons_function_" .. name .. "_time",
+		"time of " .. name .." function executes")
+
+	metrics[name] = entry
+end
+
+register_function("change");
+register_function("lc_interrupt");
+register_function("lc_digiline_relay");
+register_function("deactivate");
+register_function("activate");
+register_function("receptor_on");
+register_function("receptor_off");
+register_function("other");
 
 local old_execute = mesecon.queue.execute
 mesecon.queue.execute = function(self, action)
-	if action.func == "change" then
-		metric_function_change_count.inc()
-	elseif action.func == "lc_interrupt" then
-		metric_function_lc_interrupt_count.inc()
-	elseif action.func == "lc_digiline_relay" then
-		metric_function_lc_digiline_relay_count.inc()
-	elseif action.func == "deactivate" then
-		metric_function_deactivate_count.inc()
-	elseif action.func == "activate" then
-		metric_function_activate_count.inc()
-	elseif action.func == "receptor_on" then
-		metric_function_receptor_on_count.inc()
-	elseif action.func == "receptor_off" then
-		metric_function_receptor_off_count.inc()
-	else
-		metric_function_other.inc()
+
+        local t0 = minetest.get_us_time()
+	old_execute(self, action)
+        local t1 = minetest.get_us_time()
+	local delta_t = t1 - t0
+
+	local metric = metrics[action.func]
+	if not metric then
+		metric = metrics["other"]
 	end
 
-	old_execute(self, action)
+	metric.count.inc()
+	metric.time.inc(delta_t)
 end
