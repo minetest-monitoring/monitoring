8743fbed2241a2097134c8984f01551280f345d8
BuckarooBanzay <BuckarooBanzay@github.com>
2020-02-10 10:05:00 +0100

    more engine metrics

--- monitoring/builtin/engine.lua
+++ monitoring/builtin/engine.lua
@@ -1,9 +1,30 @@
 -- engine stats
 -- needs patch: https://github.com/pandorabox-io/minetest_docker/blob/master/patches/lua_profiler.patch
 
-local async_run_step = monitoring.gauge("engine_async_run_step", "async run step time average in milliseconds")
-local sendblocks = monitoring.counter("engine_sendblocks", "sendblocks time sum in milliseconds")
-local network_processing = monitoring.counter("engine_network_processing", "sendblocks time sum in milliseconds")
+local async_run_step = monitoring.gauge(
+	"engine_async_run_step",
+	"async run step time average in milliseconds"
+)
+
+local sendblocks = monitoring.counter(
+	"engine_sendblocks",
+	"sendblocks time sum in milliseconds"
+)
+
+local sendblocks_collect = monitoring.counter(
+	"engine_sendblocks_collect",
+	"sendblocks collect time sum in milliseconds"
+)
+
+local network_processing = monitoring.counter(
+	"engine_network_processing",
+	"sendblocks time sum in milliseconds"
+)
+
+local map_saving = monitoring.counter(
+	"engine_map_saving",
+	"map saving time sum in milliseconds"
+)
 
 local timer = 0
 minetest.register_globalstep(function(dtime)
@@ -13,5 +34,7 @@ minetest.register_globalstep(function(dtime)
 
 	async_run_step.set(minetest.get_profiler_value("Server::AsyncRunStep() [ms]"))
 	sendblocks.set(minetest.get_profiler_value("Server::SendBlocks(): Send to clients [ms]"))
+	sendblocks_collect.set(minetest.get_profiler_value("Server::SendBlocks(): Collect list [ms]"))
 	network_processing.set(minetest.get_profiler_value("Server: Process network packet (sum) [ms]"))
+	map_saving.set(minetest.get_profiler_value("Server: map saving (sum) [ms]"))
 end)
