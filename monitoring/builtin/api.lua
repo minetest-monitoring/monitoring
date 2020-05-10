73ac3e96c8c90a05d0bec584ba7f09a48d7233f7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-05-10 14:48:51 +0200

    add some builtin functions to the default monitoring level

--- monitoring/builtin/api.lua
+++ monitoring/builtin/api.lua
@@ -1,4 +1,9 @@
 
+monitoring.wrap_global({"minetest", "find_nodes_in_area"}, "find_nodes_in_area")
+monitoring.wrap_global({"minetest", "find_node_near"}, "find_node_near")
+monitoring.wrap_global({"minetest", "get_objects_inside_radius"}, "get_objects_inside_radius")
+
+-- call intensive functions, only enable if explicitly set
 if minetest.settings:get_bool("monitoring.verbose") then
   monitoring.wrap_global({"minetest", "add_entity"}, "add_entity")
   monitoring.wrap_global({"minetest", "add_item"}, "add_item")
@@ -11,15 +16,9 @@ if minetest.settings:get_bool("monitoring.verbose") then
   monitoring.wrap_global({"minetest", "get_node_or_nil"}, "get_node_or_nil")
   monitoring.wrap_global({"minetest", "get_meta"}, "get_meta")
 
-
-  monitoring.wrap_global({"minetest", "find_node_near"}, "find_node_near")
-  monitoring.wrap_global({"minetest", "find_nodes_in_area"}, "find_nodes_in_area")
-
   monitoring.wrap_global({"minetest", "swap_node"}, "swap_node")
   monitoring.wrap_global({"minetest", "load_area"}, "load_area")
   monitoring.wrap_global({"minetest", "get_voxel_manip"}, "get_voxel_manip")
-
-  monitoring.wrap_global({"minetest", "get_objects_inside_radius"}, "get_objects_inside_radius")
 
   monitoring.wrap_global({"minetest", "set_node"}, "set_node")
   monitoring.wrap_global({"minetest", "find_path"}, "find_path")
