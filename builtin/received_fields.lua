425ccd69525dc7e49dc3aeb2d8e7c287dc720706
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-10-29 22:24:34 +0100

    add on_receive_fields metric

--- builtin/received_fields.lua
+++ builtin/received_fields.lua
@@ -3,3 +3,29 @@ local metric = monitoring.counter("received_fields", "received client fields cou
 minetest.register_on_player_receive_fields(function()
 	metric.inc()
 end)
+
+
+local metric_time = monitoring.counter("on_player_receive_fields_time", "time usage in microseconds" ..
+	"for on_player_receive_fields calls")
+local metric_time_max = monitoring.gauge(
+	"on_player_receive_fields_time_max",
+	"max time usage in microseconds for on_player_receive_fields calls",
+	{ autoflush=true }
+)
+
+
+minetest.register_on_mods_loaded(function()
+  for i, fn in ipairs(minetest.registered_on_player_receive_fields) do
+
+		minetest.registered_on_player_receive_fields[i] = function(...)
+			local t0 = minetest.get_us_time()
+			fn(...)
+			local t1 = minetest.get_us_time()
+
+			local diff = t1 - t0
+      metric_time.inc(diff)
+      metric_time_max.setmax(diff)
+		end
+
+  end
+end)
