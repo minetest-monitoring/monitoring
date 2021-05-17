932b5f77b50fbc6f9837807040a3d64abf7243c7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2021-05-17 11:36:24 +0200

    intercept minetest.after() calls

---
+++ builtin/after.lua
@@ -0,0 +1,10 @@
+local metric = monitoring.counter("minetest_after_count", "number of after() calls")
+local metric_time = monitoring.counter("minetest_after_time", "time usage in microseconds for after() calls")
+
+local old_minetest_after = minetest.after
+
+minetest.after = function(delay, callback, ...)
+    callback = metric_time.wraptime(callback)
+    metric.inc()
+    old_minetest_after(delay, callback, ...)
+end
