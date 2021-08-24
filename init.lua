66ee08d1760921e965ab0cf9b95e4e996771b8d5
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2021-08-24 16:30:17 +0200

    Fixed Undeclared global variable QoS error
    Fixed the Warning message in console: 
    
    2021-08-24 07:32:18: WARNING[Main]: Undeclared global variable "QoS" accessed at /home/minetest/minetest/mods/monitoring/init.lua:18

--- init.lua
+++ init.lua
@@ -15,8 +15,7 @@ monitoring = {
   }
 }
 
-local http = QoS and QoS(minetest.request_http_api(), 2) or minetest.request_http_api()
-local MP = minetest.get_modpath("monitoring")
+local http = minetest.get_modpath("qos") and QoS and QoS(minetest.request_http_api(), 2) or minetest.request_http_api()
 
 dofile(MP.."/metrictypes/gauge.lua")
 dofile(MP.."/metrictypes/counter.lua")
