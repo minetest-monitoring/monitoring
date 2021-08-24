bb2c02b079dddd4c2270c3a9b4475418afbc120e
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2021-08-24 16:32:55 +0200

    by accident remove MP
    added: 
    local MP = minetest.get_modpath("monitoring")

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ monitoring = {
 }
 
 local http = minetest.get_modpath("qos") and QoS and QoS(minetest.request_http_api(), 2) or minetest.request_http_api()
+local MP = minetest.get_modpath("monitoring")
 
 dofile(MP.."/metrictypes/gauge.lua")
 dofile(MP.."/metrictypes/counter.lua")
