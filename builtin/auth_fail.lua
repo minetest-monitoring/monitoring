48dbd452ac0d4a129c30d424ecb489866c91a8ce
Buckaroo Banzai <39065740+BuckarooBanzay@users.noreply.github.com>
2021-08-24 15:26:58 +0200

    fix luacheck

--- builtin/auth_fail.lua
+++ builtin/auth_fail.lua
@@ -2,7 +2,7 @@ if minetest.register_on_auth_fail then
   local metric = monitoring.counter("auth_fail_count", "number of auth fails")
 
 
-  minetest.register_on_authplayer(function(name, ip, is_success)
+  minetest.register_on_authplayer(function(_, _, is_success)
     if is_success == false then
       metric.inc()
     end
