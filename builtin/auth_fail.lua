06d3b73f1b60b4be7c113cf5bc5f097b5772b972
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2021-08-24 11:37:51 +0200

    Fixed deprecated error at the auth_fail.lua file

--- builtin/auth_fail.lua
+++ builtin/auth_fail.lua
@@ -2,7 +2,9 @@ if minetest.register_on_auth_fail then
   local metric = monitoring.counter("auth_fail_count", "number of auth fails")
 
 
-  minetest.register_on_auth_fail(function()
-    metric.inc()
+  minetest.register_on_authplayer(function(name, ip, is_success)
+    if is_success == false then
+      metric.inc()
+    end
   end)
 end
