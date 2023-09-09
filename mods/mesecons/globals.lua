625d543bbb02c10bcb6e680d40f23e7aa4fa8382
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-09-09 18:02:01 +0200

    monitor `mesecon.vm_*` calls in non-verbose mode

--- mods/mesecons/globals.lua
+++ mods/mesecons/globals.lua
@@ -13,8 +13,11 @@ if monitoring.settings.handle_errors then
   end
 end
 
-if minetest.settings:get_bool("monitoring.mesecons.verbose") then
+monitoring.wrap_global({"mesecon", "vm_begin"}, "mesecons_vm_begin")
+monitoring.wrap_global({"mesecon", "vm_abort"}, "mesecons_vm_abort")
+monitoring.wrap_global({"mesecon", "vm_commit"}, "mesecons_vm_commit")
 
+if minetest.settings:get_bool("monitoring.mesecons.verbose") then
   monitoring.wrap_global({"mesecon", "get_node_force"}, "mesecons_get_node_force")
   monitoring.wrap_global({"mesecon", "activate"}, "mesecons_activate")
   monitoring.wrap_global({"mesecon", "deactivate"}, "mesecons_deactivate")
@@ -22,9 +25,4 @@ if minetest.settings:get_bool("monitoring.mesecons.verbose") then
   monitoring.wrap_global({"mesecon", "turnoff"}, "mesecons_turnoff")
   monitoring.wrap_global({"mesecon", "changesignal"}, "mesecons_changesignal")
   monitoring.wrap_global({"mesecon", "swap_node_force"}, "swap_node_force")
-
-  monitoring.wrap_global({"mesecon", "vm_begin"}, "mesecons_vm_begin")
-  monitoring.wrap_global({"mesecon", "vm_abort"}, "mesecons_vm_abort")
-  monitoring.wrap_global({"mesecon", "vm_commit"}, "mesecons_vm_commit")
-
 end
