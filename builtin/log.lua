7d58e71749b81896231c2be993e2b2ea097e074f
Niklp <nik@niklp.net>
2024-01-19 15:48:15 +0100

    Make interception of minetest.log generic

--- builtin/log.lua
+++ builtin/log.lua
@@ -2,12 +2,7 @@ local metric = monitoring.counter("log_count", "number of log messages")
 
 local old_log = minetest.log
 
-function minetest.log(level, text)
-    metric.inc()
-    -- The number and order of parameters is important here
-    if text ~= nil then
-        return old_log(level, text)
-    else
-        return old_log(level)
-    end
+function minetest.log(...)
+	metric.inc()
+	return old_log(...)
 end
