a1b1e41593ce01a49ce390683894e4e7fe1a294b
мтест <52937829+waxtatect@users.noreply.github.com>
2025-04-19 14:58:02 +0200

    update basic_machines mod (#35)

--- mods/basic_machines/init.lua
+++ mods/basic_machines/init.lua
@@ -12,15 +12,13 @@ local function register_effector_metric(nodename, metricname, prettyname)
 	if nodedef.effector and nodedef.effector.action_on then
 		nodedef.effector.action_on = metric_count.wrap( metric_time.wraptime(nodedef.effector.action_on) )
 	end
-	if nodedef.mesecons and nodedef.mesecons.effector and nodedef.mesecons.effector.action_on then
-		nodedef.mesecons.effector.action_on = metric_count.wrap( metric_time.wraptime(nodedef.mesecons.effector.action_on) )
-	end
 end
 
+register_effector_metric("basic_machines:autocrafter", "autocrafter", "Autocrafter")
+register_effector_metric("basic_machines:ball_spawner", "ball_spawner", "Ball Spawner")
 register_effector_metric("basic_machines:mover", "mover", "Mover")
 register_effector_metric("basic_machines:keypad", "keypad", "Keypad")
 register_effector_metric("basic_machines:detector", "detector", "Detector")
-register_effector_metric("basic_machines:clockgen", "clockgen", "Clockgen")
 register_effector_metric("basic_machines:distributor", "distributor", "Distributor")
 register_effector_metric("basic_machines:light_off", "light_off", "Light off")
 register_effector_metric("basic_machines:light_on", "light_on", "Light on")
